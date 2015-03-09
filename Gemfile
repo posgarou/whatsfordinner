6ffe2bc9eeab1798c4c36e636d2347d4871eb9ec
Ryan Mitchell <ryan@napaswdev.com>
2015-03-09 11:09:11 -0400

    Integration tests set up.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,11 @@
 
 gem 'rack'
 gem 'rack-mount'
+gem 'rack-test'
+gem 'test-unit'
+gem 'capybara', require: ['capybara', 'capybara/dsl']
+gem 'rspec'
+gem 'selenium-webdriver'
 
 # Include the client and server gemfiles
 CLIENT_GEMFILE_PATH = './client/Gemfile'
---
+++ spec/index_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'spec_helper'
+
+describe 'The front page', type: :feature, js: true do
+  
+  it 'loads the test page successfully' do
+    visit '/'
+    expect(page).to have_content 'Hi there'
+  end
+
+  it 'loads the example instructions successfully' do
+    visit '/#instructions'
+    expect(page).to have_content 'HUEVOS RANCHEROS'
+  end
+end
