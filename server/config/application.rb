1474608d64608de17e62e2ca62c860ea1034535e
Ryan Mitchell <ryan@napaswdev.com>
2015-03-10 15:20:23 -0400

    Add Mongoid, and correct Rails config so it can rackup independently for rake.

--- server/config/application.rb
+++ server/config/application.rb
@@ -34,7 +34,7 @@ module WFDinnerServer
 
     Rails.application.config.middleware.use OmniAuth::Builder do
       provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-      provider :google, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_SECRET']
+      provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_SECRET']
     end
   end
 end
