e6dcee86134b92b2c80d8b500ea7997a4db7bee0
Ryan Mitchell <ryan@napaswdev.com>
2015-03-10 16:50:00 -0400

    Mongoid config.

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,6 +1,8 @@
 class User
   include Mongoid::Document
 
+  field :name, type: String
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
