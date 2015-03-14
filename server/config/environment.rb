79c46d9fd35f6ed9996939dab32f89878c6046d7
Ryan Mitchell <ryan@napaswdev.com>
2015-03-14 19:21:53 -0400

    Minor cleanup.

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -4,7 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Default to RACK_ENV or, if this is not set, 'development'
-ENV['RAILS_ENV'] ||= ENV['RACK_ENV'] || 'development'
-
 ENV['ROOT_URL'] ||= 'http://localhost:3000'
