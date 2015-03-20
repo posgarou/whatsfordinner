b569058947edd32a3c4a27adfdebf73c0a9431be
Ryan Mitchell <ryan@napaswdev.com>
2015-03-20 11:54:14 -0400

    Correct failing Tag spec.

--- server/spec/models/tag_spec.rb
+++ server/spec/models/tag_spec.rb
@@ -1,7 +1,7 @@
 describe Graph::Tag do
   it 'saves successfully' do
     expect{ FactoryGirl.create(:tag) }
-      .to change{ Graph::Tag.count }.from(0).to(1)
+      .to change{ Graph::Tag.count }.by(1)
   end
 
   it 'sets up relationships correctly' do
