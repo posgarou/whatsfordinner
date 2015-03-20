43823d4a81b4705ff4c4225592216b6dff5b9e27
Ryan Mitchell <ryan@napaswdev.com>
2015-03-20 02:24:19 -0400

    Add Rejected relationship between Graph::User and Recipe.

---
+++ server/app/models/graph/recipe_interactions/rejected.rb
@@ -0,0 +1,13 @@
+module Graph
+  module RecipeInteractions
+    # Represents a rejected event.
+    #
+    # A rejection occurs when another Recipe is chosen instead of this rel's Recipe.
+    class Rejected
+      include Graph::RecipeInteractions::Base
+      include Grape::Entity::DSL
+
+      type 'RATED'
+    end
+  end
+end
